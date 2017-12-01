b6be9fdf1b1091c5c26f6bb2468dff117336bf23
awbridgers <awbridgers@users.noreply.github.com>
2017-11-30 23:51:22 -0500

    working version of the main data for all the games

--- src/App.js
+++ src/App.js
@@ -1,18 +1,89 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import config from './config.js'
+import * as firebase from 'firebase'
+
+
+
+
+let testArray = (array) => array.forEach((x) => console.log(x));
+class Data {
+  constructor(lineup, value) {
+    this.lineup = lineup;
+    this.value = value;
+  }
+}
+
+let findLineup = (array, lineup) => {
+  let index = -1
+  array.forEach((x,i) => {
+    if(x.lineup === lineup)
+    {
+      index = i;
+    }
+  });
+  return index;
+}
 
 class App extends Component {
+  constructor(){
+    super();
+    this.db = firebase.initializeApp(config);
+    this.ref = this.db.database().ref();
+    this.state = {dataArray: []};
+    this.reverseOrder = this.reverseOrder.bind(this);
+    this.ascending = true;
+
+  }
+  componentDidMount(){
+     this.getData = this.ref.once('value').then((snapshot) => {
+       let array =[];
+       snapshot.forEach((childSnapshot) => {
+         childSnapshot.child('lineups').forEach((x) => {
+
+           let temp = new Data (x.key, x.val());
+           let index = findLineup(array,temp.lineup)
+           if(index === -1){
+             array.push(temp);
+           }
+           else{
+             array[index].value += temp.value
+           }
+         });
+         })
+        array.sort((a,b)=>{return a.value - b.value}).reverse();
+
+       this.setState({dataArray: array});
+     })
+  }
+  reverseOrder(){
+    this.setState({dataArray: this.state.dataArray.reverse()});
+    this.ascending = !this.ascending;
+  }
   render() {
+    console.log(this.state.dataArray.length)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table>
+          <tbody>
+            <tr>
+              <th style = {{width: "85%"}}>Lineup</th><th>
+                <button style = {{background: "#42444e", width:"100%", border: "none"}} onClick = {this.reverseOrder}><b>+&frasl;-</b></button></th>
+            </tr>
+        {this.state.dataArray.map((x,i) => {
+          return (
+            <tr key ={i}>
+              <td>{x.lineup}</td><td>{x.value}</td>
+            </tr>
+          )
+        })
+        }
+        </tbody>
+        </table>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Routing = () => (
+  <Router>
+    <div>
+    <Route exact path ="/" component = {App} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
 registerServiceWorker();
