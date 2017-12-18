dabe109461baf0c9276da5b3638089c830ed6c4a
awbridgers <awbridgers@users.noreply.github.com>
2017-12-17 23:29:56 -0500

    rewrote the time convert function so that the app would build

--- src/games.js
+++ src/games.js
@@ -17,9 +17,9 @@ const fixTime = seconds => {
   if (secs < 0) throw new Error('Seconds must be positive');
 
   if (secs < 60) {
-    if (secs < 10) return `0:0${secs}`;
+    if (secs < 10) return "0:0" + secs;
 
-    return `0:${secs}`;
+    return "0:" + secs;
   }
 
   let minuteDivisor = secs % (60 * 60);
@@ -28,15 +28,15 @@ const fixTime = seconds => {
   let secondDivisor = minuteDivisor % 60;
   let remSecs = Math.ceil(secondDivisor);
 
-  if (remSecs < 10 && remSecs > 0) remSecs = `0${remSecs}`;
-  if (remSecs === 0) remSecs = `${remSecs}0`;
+  if (remSecs < 10 && remSecs > 0) remSecs = "0" + remSecs;
+  if (remSecs === 0) remSecs = remSecs + "0";
 
   let time = {
     m: minutes,
     s: remSecs
   };
 
-  return `${time.m}:${time.s}`;
+  return time.m + ":" + time.s;
 };
 
 
